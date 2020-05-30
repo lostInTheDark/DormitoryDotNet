e79d5f19d1ab465d2e6ad22e39ee011259341012
Rookie <geralt1530@gmail.com>
2020-05-30 20:39:37 +0300

    Fixed uploading images, added password confirmation

--- DormitoryDotNet/apps/users/forms.py
+++ DormitoryDotNet/apps/users/forms.py
@@ -5,13 +5,21 @@ from django.contrib.auth.models import User
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
+    password2 = forms.CharField(widget=forms.PasswordInput())
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
 
+    def clean(self):
+        cleaned_data = super(UserForm, self).clean()
+        password = cleaned_data.get('password')
+        password2 = cleaned_data.get('password2')
+
+        if password != password2:
+            raise forms.ValidationError('Passwords do not match!')
 
 class UserProfileForm(forms.ModelForm):    
     class Meta:
         model = Profile
-        fields = ("prof_pic", "room")
+        fields = {"prof_pic", "room"}
--- DormitoryDotNet/apps/users/models.py
+++ DormitoryDotNet/apps/users/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 from DormitoryManagement.models import Room
+from django.conf import settings
 
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.SET_NULL, null=True)
     prof_pic = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
         return self.user.username
+        
 
 def delete_user(sender, instance=None, **kwargs):
     try:
