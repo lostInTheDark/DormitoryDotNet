de67e823d5a96f1108583f9516e39cc0614b747e
Rookie <geralt1530@gmail.com>
2020-06-04 07:21:22 +0300

    ui fixes

--- DormitoryDotNet/apps/DormitoryManagement/urls.py
+++ DormitoryDotNet/apps/DormitoryManagement/urls.py
@@ -8,4 +8,10 @@ urlpatterns = [
     path('blog/', views.blog, name = 'blog'),
     path('contact/', views.contact, name = 'contact'),
     path('single_post/', views.single_post, name = 'single_post'),
+    path('contact/', views.contact, name = 'contact'),
+    path('map1/', views.map1, name='map1'),
+    path('map2/', views.map2, name='map2'),
+    path('map3/', views.map3, name='map3'),
+    path('map4/', views.map4, name='map4'),
+    path('map5/', views.map5, name='map5'),
 ]
--- DormitoryDotNet/apps/DormitoryManagement/views.py
+++ DormitoryDotNet/apps/DormitoryManagement/views.py
@@ -14,3 +14,21 @@ def contact(request):
 
 def single_post(request):
     return render(request, 'DormitoryManagement/blog-single-post.html')
+
+def contact(request):
+    return render(request, 'DormitoryManagement/contact.html')
+
+def map2(request):
+    return render(request, 'DormitoryManagement/map2.html')
+
+def map3(request):
+    return render(request, 'DormitoryManagement/map3.html')
+
+def map4(request):
+    return render(request, 'DormitoryManagement/map4.html')
+
+def map5(request):
+    return render(request, 'DormitoryManagement/map5.html')
+
+def map1(request):
+    return render(request, 'DormitoryManagement/map1.html')
